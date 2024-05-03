ed5a89b358cb9c75a2c5c3f3b803ec0363664d05
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-03 14:13:37 +0200

    adding main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+import customtkinter
+import customtkinter as ctk
+
+# Appearance settings
+ctk.set_appearance_mode("System")  # Light/Dark mode
+ctk.set_default_color_theme("blue")  # Default color
+
+# Generating our frame
+main_frame = customtkinter.CTk()
+main_frame.geometry("640x480")  # Defining frame geometry
+main_frame.title("Cosmic On Air")
+
+# Running app
+main_frame.mainloop()
