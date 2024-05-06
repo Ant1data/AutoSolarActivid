a3f3f80315583f29b4b7f6834fea2efb2bf11dab
Marc Roussel <150703055+marcroussel@users.noreply.github.com>
2024-05-06 15:40:51 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-import customtkinter
 import customtkinter as ctk
 
 # Appearance settings
@@ -8,7 +7,7 @@ ctk.set_appearance_mode("System")  # Light/Dark mode
 ctk.set_default_color_theme("blue")  # Default color
 
 # Generating our frame
-main_frame = customtkinter.CTk()
+main_frame = ctk.CTk()
 main_frame.geometry("640x480")  # Defining frame geometry
 main_frame.title("Cosmic On Air")
 
