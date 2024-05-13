345e6ddcb586139a3b9f3be1edbbb70ae3aeb78c
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-13 17:36:52 +0200

    adding video type and graph button

--- app/main.py
+++ app/main.py
@@ -1,25 +1,46 @@
 import tkinter as tk
-
 import customtkinter as ctk
+import os
+
+from PIL import Image
+
+## <a href="https://www.flaticon.com/fr/icones-gratuites/analytique" title="analytique icônes">Analytique icônes créées par Arafat Uddin - Flaticon</a> ##
+
+# Path settings
+image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "img") # Path to img folder
 
 # Appearance settings
 ctk.set_appearance_mode("System")  # Light/Dark mode
 ctk.set_default_color_theme("blue")  # Default color
 
-# Generating our frame
-main_frame = ctk.CTk()
-main_frame.geometry("640x480")  # Defining frame geometry
-main_frame.title("Cosmic On Air")
+# Generating our main window
+frmMain = ctk.CTk()
+frmMain.geometry("640x480")  # Defining frame geometry
+frmMain.title("Cosmic On Air")
 
  
-## ---------- View elements ---------- ##
-frmTitleBar = ctk.CTkFrame(main_frame)
+## ---------- VIEW ELEMENTS ---------- ##
+# ----- Title Bar ----- #
+frmTitleBar = ctk.CTkFrame(frmMain)
 frmTitleBar.pack(anchor="center", fill="x")
 
-lblTitle = ctk.CTkLabel(frmTitleBar, text="COSMIC ON AIR | Video Generator")
+lblTitle = ctk.CTkLabel(frmTitleBar, text="COSMIC ON AIR | Video Generator", font=ctk.CTkFont(size=20, weight="bold"))
 lblTitle.pack(pady=10)
+
+
+# ----- Video Type Frame ----- #
+frmVideoType = ctk.CTkFrame(frmMain)
+frmVideoType.pack(anchor="center", fill="x", pady=10)
+
+lblVideoType = ctk.CTkLabel(frmVideoType, text="Video type")
+lblVideoType.pack(padx=8, pady=8, anchor="w")
+
+icnGraph = ctk.CTkImage(Image.open(os.path.join(image_path, "graph.png")))
+
+btnProtonFluxGraph = ctk.CTkButton(frmVideoType, text="Proton flux graph", image=icnGraph, compound="top")
+btnProtonFluxGraph.pack()
 
 
 
 # Running app
-main_frame.mainloop()
+frmMain.mainloop()
