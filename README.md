dc72492a6b8e81cb7580a715b30c575d3f930194
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-14 17:26:37 +0200

    finshied video frame and beginning timestamp frame

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Provenance des icônes
 [Analytique icônes créées par Arafat Uddin - Flaticon](https://www.flaticon.com/fr/icones-gratuites/analytique)
+
+[Lumière du soleil icônes créées par Vectoricons - Flaticon](https://www.flaticon.com/fr/icones-gratuites/lumiere-du-soleil)
--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+import os
 import tkinter as tk
 import customtkinter as ctk
-import os
 
+from datetime import date
 from PIL import Image
+from tkcalendar import Calendar
+
 
 ## <a href="https://www.flaticon.com/fr/icones-gratuites/analytique" title="analytique icônes">Analytique icônes créées par Arafat Uddin - Flaticon</a> ##
 
-# Path settings
-image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "img") # Path to img folder
+# 
+img_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "img")
 
 # Appearance settings
 ctk.set_appearance_mode("System")  # Light/Dark mode
@@ -35,11 +38,20 @@ frmVideoType.pack(anchor="center", fill="x", pady=10)
 lblVideoType = ctk.CTkLabel(frmVideoType, text="Video type")
 lblVideoType.pack(padx=8, pady=8, anchor="w")
 
-icnGraph = ctk.CTkImage(Image.open(os.path.join(image_path, "graph.png")))
+icnGraph = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "graph.png")))
 
 btnProtonFluxGraph = ctk.CTkButton(frmVideoType, text="Proton flux graph", image=icnGraph, compound="top")
-btnProtonFluxGraph.pack()
+btnProtonFluxGraph.pack(side="left", expand=True, fill=tk.X, padx=16, pady=8)
 
+icnSun = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "sun.png")))
+
+btnSolarActivityVideo = ctk.CTkButton(frmVideoType, text="Solar activity video", image=icnSun, compound="top")
+btnSolarActivityVideo.pack(side="left", expand=True, fill=tk.X, padx=16)
+
+
+# ----- Timestamp Frame ----- #
+frmTimestamp = ctk.CTkFrame(frmMain)
+frmTimestamp.pack(anchor="center", fill="x", pady=10)
 
 
 # Running app
