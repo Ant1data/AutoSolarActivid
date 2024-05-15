992887f87af603f2e37788ad8410b1f4c906a526
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-15 11:46:06 +0200

    adding calendar and configuring some grid layouts

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from tkcalendar import Calendar
 
 ## <a href="https://www.flaticon.com/fr/icones-gratuites/analytique" title="analytique icônes">Analytique icônes créées par Arafat Uddin - Flaticon</a> ##
 
-# 
+# Path to app/img folder, in order to get image files 
 img_folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "img")
 
 # Appearance settings
@@ -22,37 +22,71 @@ frmMain.geometry("640x480")  # Defining frame geometry
 frmMain.title("Cosmic On Air")
 
  
-## ---------- VIEW ELEMENTS ---------- ##
+## VIEW ELEMENTS ----------------------------------------------------------------------------------------------------------- ##
 # ----- Title Bar ----- #
 frmTitleBar = ctk.CTkFrame(frmMain)
 frmTitleBar.pack(anchor="center", fill="x")
 
+# Title Label
 lblTitle = ctk.CTkLabel(frmTitleBar, text="COSMIC ON AIR | Video Generator", font=ctk.CTkFont(size=20, weight="bold"))
 lblTitle.pack(pady=10)
 
 
 # ----- Video Type Frame ----- #
 frmVideoType = ctk.CTkFrame(frmMain)
+
+# Configuring grid layout for this frame
+frmVideoType.columnconfigure((0,1), weight=1)
+frmVideoType.rowconfigure(0, weight=1)
+frmVideoType.rowconfigure(1, weight=3)
 frmVideoType.pack(anchor="center", fill="x", pady=10)
 
 lblVideoType = ctk.CTkLabel(frmVideoType, text="Video type")
-lblVideoType.pack(padx=8, pady=8, anchor="w")
+lblVideoType.grid(row=0, column=0, sticky="w", padx=8)
 
-icnGraph = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "graph.png")))
+# Proton Flux Graph Button
+icnGraph = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "graph.png"))) # Defining icon
+btnProtonFluxGraph = ctk.CTkButton(frmVideoType, text="Proton flux graph", image=icnGraph, compound="top", cursor="hand2")
+btnProtonFluxGraph.grid(row=1, column=0, sticky="e", padx=8)
 
-btnProtonFluxGraph = ctk.CTkButton(frmVideoType, text="Proton flux graph", image=icnGraph, compound="top")
-btnProtonFluxGraph.pack(side="left", expand=True, fill=tk.X, padx=16, pady=8)
-
-icnSun = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "sun.png")))
-
-btnSolarActivityVideo = ctk.CTkButton(frmVideoType, text="Solar activity video", image=icnSun, compound="top")
-btnSolarActivityVideo.pack(side="left", expand=True, fill=tk.X, padx=16)
+# Solar Activity Video Button
+icnSun = ctk.CTkImage(Image.open(os.path.join(img_folder_path, "sun.png"))) # Defining icon
+btnSolarActivityVideo = ctk.CTkButton(frmVideoType, text="Solar activity video", image=icnSun, compound="top", cursor="hand2")
+btnSolarActivityVideo.grid(row=1, column=1, sticky="w", padx=8)
 
 
 # ----- Timestamp Frame ----- #
 frmTimestamp = ctk.CTkFrame(frmMain)
+
+# Configuring grid layout for this frame
+frmTimestamp.columnconfigure((0,1), weight=1)
+frmTimestamp.rowconfigure(0, weight=1)
+frmTimestamp.rowconfigure(1, weight=3)
 frmTimestamp.pack(anchor="center", fill="x", pady=10)
 
+# Timestamp label
+lblTimestamp = ctk.CTkLabel(frmTimestamp, text="Timestamp")
+lblTimestamp.grid(row=0, column=0, sticky="w", padx=8)
+
+
+# --- Begin Frame --- #
+frmBegin = ctk.CTkFrame(frmTimestamp)
+
+# Configuring grid layout for Begin frame
+frmBegin.columnconfigure((0,2,4), weight=2)
+frmBegin.columnconfigure((1,3), weight=1)
+frmBegin.rowconfigure((0,2), weight=1)
+frmBegin.rowconfigure(1, weight=2)
+frmBegin.grid(row=1, column=0, sticky="e", padx=8) # On the Timestamp grid
+
+# Begin label
+lblBegin = ctk.CTkLabel(frmBegin, text="Begin")
+lblBegin.grid(row=0, column=2)
+
+cldrBegin = Calendar(frmBegin, cursor="hand2")
+cldrBegin.grid(row=1, column=2)
+
+## TODO : Mettre certaines Frames dans des classes à part entiere
 
 # Running app
 frmMain.mainloop()
