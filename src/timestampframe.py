fef15d69473954761422c740e189cd04b899c45a
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-16 17:18:06 +0200

    configuring grid for timestampframe

--- src/timestampframe.py
+++ src/timestampframe.py
@@ -9,6 +9,13 @@ class TimestampFrame(ctk.CTkFrame):
     def __init__(self, root, **kwargs):
         super().__init__(root, **kwargs)
         self.root = root # Defining tkinter root container
-        self.calendar = Calendar()
+
+        # Grid configuration
+        self.columnconfigure((0,2,4), weight=3)
+        self.columnconfigure((1,3), weight=1)
+        self.rowconfigure(0, weight=3)
+        self.rowconfigure(1, weight=1)
         
-    # TODO : Create remaining elements
+        # Adding elements
+        self.calendar = Calendar() # TODO : put correct arguments inside
+        self.grid(row=0, column=0) # rowstamp or smth like this remaining
