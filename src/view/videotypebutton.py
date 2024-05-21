9a7dfea6d18fbc9e3df72505336a09623aadbb05
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-21 16:11:44 +0200

    beginning videotypebutton class

---
+++ src/view/videotypebutton.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import customtkinter as ctk
+
+class VideoTypeButton(ctk.CTkButton):
+
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
+        
