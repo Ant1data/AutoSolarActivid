a6f4cd175a23c0a2a7a6e2496bb07c74d4afce9f
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-05-27 16:04:30 +0200

    configuring irsn proxy

--- src/model/protonfluxdata.py
+++ src/model/protonfluxdata.py
@@ -6,5 +6,9 @@ class ProtonFluxData:
         pass
 
 ## ---------- TEST ZONE ---------- ##
-http_request = requests.get("https://services.swpc.noaa.gov/json/goes/primary/differential-protons-1-day.json")
-print(http_request.json)
+
+# Proxy configuration
+
+irsn_proxy = {"https" : "http://roussel-mar:boqpag-mijxi2-Duhdyk@wpad.proton.intra.irsn.fr:3128"}
+http_request = requests.get(url="https://services.swpc.noaa.gov/json/goes/primary/differential-protons-1-day.json", proxies=irsn_proxy)
+print(http_request.json())
