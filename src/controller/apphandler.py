03d123aae4439df45f95ab9fa6ed19bd6c886bfb
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-06-14 17:44:59 +0200

    adding AppHandler

--- src/controller/apphandler.py
+++ src/controller/apphandler.py
@@ -1,3 +1,18 @@
+import customtkinter as ctk
+import tkinter as tk
+
 class AppHandler():
+
     def __init__(self):
-        pass
+        # Appearance settings
+        ctk.set_appearance_mode("System")  # Light/Dark mode
+        ctk.set_default_color_theme("blue")  # Default color
+        
+        # Creating main window
+        self.main_window = ctk.CTk()
+        self.main_window.geometry("800x800")
+        self.main_window.minsize(500, 375)
+        self.main_window.title("SolarActivid")
+
+        # Launching app
+        self.main_window.mainloop()
