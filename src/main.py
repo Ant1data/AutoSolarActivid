065a33d924e460a2a8322ef60fb5a909fc17459e
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-06-18 09:28:37 +0200

    making main.py call AppHandler

--- src/main.py
+++ src/main.py
@@ -1,20 +1,7 @@
 import customtkinter as ctk
 
-from view.appframe import AppFrame
+from controller.apphandler import AppHandler
 
-# # Appearance settings
-# ctk.set_appearance_mode("System")  # Light/Dark mode
-# ctk.set_default_color_theme("blue")  # Default color
-
-# # Creating main
-# main_window = ctk.CTk()
-# main_window.geometry("800x800")
-# main_window.minsize(500, 375)
-# main_window.title("SolarActivid")
-
-# # Adding app frame on main window
-# app_frame = AppFrame(main_window, width=1200, height=1400, fg_color="white")
-# app_frame.pack()
-
-# # Lauching app
-# main_window.mainloop()
+# Main function
+if __name__ == "__main__":
+    AppHandler()
