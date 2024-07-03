10bfcdaab165ed11feb9351ca48563fea84c338a
ROUSSEL Marc <marc.roussel@irsn.fr>
2024-07-03 17:16:22 +0200

    begin of ParticleFluxGraphImages

--- src/model/particlefluxgraphimages.py
+++ src/model/particlefluxgraphimages.py
@@ -4,4 +4,10 @@ class ParticleFluxGraphImages():
 
     # Constructor that will directly build the graph images,
     def __init__(self, beginDateTime : datetime, endDateTime : datetime, dctEnergy : dict[str, bool]):
-        pass
+        
+        self.beginDateTime = beginDateTime
+        self.endDateTime = endDateTime
+        self.dctEnergy = dctEnergy
+
+        
+
