5fb3eb06d07a22c5c577ec557e6d1345675ae597
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2024-07-31 17:00:14 +0200

    adding FolderPathFrame class

--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,10 +1,11 @@
 from .appframe import AppFrame
 from .commentframe import CommentFrame
 from .energyframe import EnergyFrame
+from .folderpathsframe import FolderPathsFrame
 from .formatqualityframe import FormatQualityFrame
 from .timestampframe import TimestampFrame
 from .titlebar import TitleBar
 from .videotypebutton import VideoTypeButton
 from .videotypeframe import VideoTypeFrame
 
-__all__ = ['AppFrame', 'CommentFrame', 'EnergyFrame', 'FormatQualityFrame', 'TimestampFrame', 'TitleBar', 'VideoTypeButton', 'VideoTypeFrame']
+__all__ = ['AppFrame', 'CommentFrame', 'EnergyFrame', 'FolderPathsFrame', 'FormatQualityFrame', 'TimestampFrame', 'TitleBar', 'VideoTypeButton', 'VideoTypeFrame']
---
+++ src/view/folderpathsframe.py
@@ -0,0 +1,55 @@
+import customtkinter as ctk
+
+from tkinter import filedialog
+
+class FolderPathsFrame(ctk.CTkFrame):
+
+    ## CONSTRUCTOR --------------------------------------------------------------------------------------------------------- ##
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # Grid configuration
+        self.columnconfigure((0, 1, 2), weight=1)
+        self.rowconfigure((0, 1, 2), weight=1)
+
+        # Folder paths label
+        self.lblFolderPaths = ctk.CTkLabel(self, text="Folder paths")
+        self.lblFolderPaths.grid(row=0, column=0, sticky="w", padx=8)
+
+        # Input folder path
+        self.lblInputPath = ctk.CTkLabel(self.frmVideo, text="Input folder path:")
+        self.lblInputPath.grid(row=1, column=0, padx=10, pady=5, sticky="e")
+
+        self.entInputPath = ctk.CTkEntry(self.frmVideo, width=250)
+        self.entInputPath.grid(row=1, column=1, padx=10, pady=5)
+
+        self.btnBrowseInput = ctk.CTkButton(self.frmVideo, text="Browse", command=self.browse_input_folder)
+        self.btnBrowseInput.grid(row=1, column=2, padx=10, pady=5)
+
+        # Output folder path
+        self.lblOutputPath = ctk.CTkLabel(self.frmVideo, text="Output folder path:")
+        self.lblOutputPath.grid(row=2, column=0, padx=10, pady=5, sticky="e")
+
+        self.entOutputPath = ctk.CTkEntry(self.frmVideo, width=250)
+        self.entOutputPath.grid(row=2, column=1, padx=10, pady=5)
+
+        self.btnBrowseOutput = ctk.CTkButton(self.frmVideo, text="Browse", command=self.browse_output_folder)
+        self.btnBrowseOutput.grid(row=2, column=2, padx=10, pady=5)
+
+    ## METHODS ------------------------------------------------------------------------------------------------------------- ##
+
+    ## This function, triggered by btnBrowseInput, opens a dialog window to choose the input folder
+    ## where solar activity images are stored
+    def browse_input_folder(self):
+        folder_path = filedialog.askdirectory(title="Choose input folder...")
+        if folder_path:
+            self.entInputPath.delete(0, ctk.END)
+            self.entInputPath.insert(0, folder_path)
+
+    ## This function, triggered by btnBrowseOutput, opens a dialog window to choose the output folder
+    ## where rendered videos will be stored
+    def browse_output_folder(self):
+        folder_path = filedialog.askdirectory(title="Choose output folder...")
+        if folder_path:
+            self.entOutputPath.delete(0, ctk.END)
+            self.entOutputPath.insert(0, folder_path)
