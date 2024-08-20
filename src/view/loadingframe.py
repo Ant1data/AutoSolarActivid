bd69a036c8661a5ed2ed6b758f73bb3eda9216a9
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2024-08-20 10:43:08 +0200

    removing test zone from LoadingFrame

--- src/view/loadingframe.py
+++ src/view/loadingframe.py
@@ -35,20 +35,3 @@ class LoadingFrame(ctk.CTkFrame):
 
         # Updating Percentage label
         self.lblPercentage._text = str(int((current_step/max_steps)*100)) + "%"
-    
-
-## ---------- TEST ZONE ---------- ##
-# Appearance settings
-ctk.set_appearance_mode("System")  # Light/Dark mode
-ctk.set_default_color_theme("blue")  # Default color
-
-# Creating main window
-main_window = ctk.CTk()
-main_window.geometry("800x600")
-main_window.minsize(500, 375)
-main_window.title("SolarActivid - LoadingFrame test")
-
-test_loading_frame = LoadingFrame(master=main_window, fg_color="transparent")
-test_loading_frame.pack()
-
-main_window.mainloop()
