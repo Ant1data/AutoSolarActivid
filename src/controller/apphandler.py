154408c91fe3064c251eeb75c4add013e17fdb69
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2025-07-01 10:59:47 +0200

    refacto: reorganizing constants into a standalone file

--- src/controller/apphandler.py
+++ src/controller/apphandler.py
@@ -10,32 +10,11 @@ from PIL import Image, ImageDraw, ImageFont
 from queue import Queue
 from threading import Thread
 
+from common.constants import *
 from model.particlefluxgraphimages import ParticleFluxGraphImages
 from model.solaractivityimages import SolarActivityImages
 from view.appframe import AppFrame
 from view.loadingframe import LoadingFrame
-
-
-## CONSTANTS --------------------------------------------------------------------------------------------------------- ##
-# Screen formats
-HORIZONTAL = "h"
-VERTICAL = "v"
-
-# Screen resolutions for videos
-RESOLUTION_HORIZONTAL_MED = (1280, 720)
-RESOLUTION_VERTICAL_MED = (720, 1280)
-RESOLUTION_HORIZONTAL_HIGH = (1920, 1080)
-RESOLUTION_VERTICAL_HIGH = (1080, 1920)
-
-# Comment block height
-COMMENT_BLOCK_HEIGHT = 60
-
-# Signals for loading frame
-BREAK_LOOP = -1
-UPDATE_STEP = 1
-UPDATE_PERCENTAGE = 2
-## ------------------------------------------------------------------------------------------------------------------- ##
-
 
 
 class AppHandler():
